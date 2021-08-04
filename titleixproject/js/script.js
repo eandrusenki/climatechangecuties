8245fea6b88a565ca1b5a430c292ee0d28a0580c
ramirezirvin <irvinramirez96@gmail.com>
2021-08-03 18:16:12 -0700

    map fix (3)

--- titleixproject/js/script.js
+++ titleixproject/js/script.js
@@ -1,9 +1,10 @@
 const myMap = L.map('mapArea').setView([34.0709, -118.444], 6);
-
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}).addTo(myMap)
+let Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+	attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+	maxZoom: 16
+});
+Esri_WorldGrayCanvas.addTo(myMap);
+let url = "https://spreadsheets.google.com/feeds/list/1ZH2-pBKLWZEDFkV90ECcUX5HMw4P-8vpKjDxNs3wnMc/okrozsk/public/values?alt=json"
 
 function populateContent(data){
   contentParts = {
