05d5029c05836ac8ddd6528b7a9164e0b6a4e8f0
ramirezirvin <irvinramirez96@gmail.com>
2021-08-03 17:59:22 -0700

    map

--- titleixproject/js/script.js
+++ titleixproject/js/script.js
@@ -1,10 +1,9 @@
 const myMap = L.map('mapArea').setView([34.0709, -118.444], 6);
 
-let Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
+var Stadia_AlidadeSmooth = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png', {
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-}); 
-
+}).addTo(myMap)
 
 function populateContent(data){
   contentParts = {
